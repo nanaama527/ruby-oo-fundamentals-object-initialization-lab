4f67a87a6d86787743380ab744466d433bdc3558
Nana <72888369+nanaama527@users.noreply.github.com>
2021-01-15 18:58:53 -0500

    commit tree 8b519850949918279edea16211515387ab22244c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,23 @@
 class Dog
-    def initialize(name, breed)
+    def initialize(name, breed='Mutt' )
         @name = name
+        @breed = breed
     end
     
-    def name=(name)
-        @name = name
+    def name=(new_name)
+        @name = new_name
     end
 
     def name
         @name
     end 
+    
+    def breed=(new_breed)
+        @breed = new_breed
+    end
+
+    def breed
+        @breed
+    end
 end
 
